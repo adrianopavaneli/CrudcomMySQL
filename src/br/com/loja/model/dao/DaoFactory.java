213c51539488ab3219e7fadd7a4d7d0988d40051
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-05-31 21:58:08 -0300

    Correção Final

--- src/br/com/loja/model/dao/DaoFactory.java
+++ src/br/com/loja/model/dao/DaoFactory.java
@@ -1,15 +1,11 @@
 package br.com.loja.model.dao;
 
 import br.com.loja.db.DB;
-import br.com.loja.model.dao.impl.AgendaDaoJDBC;
 import br.com.loja.model.dao.impl.DepartamentosDaoJDBC;
 import br.com.loja.model.dao.impl.VendedoresDaoJDBC;
 
 public class DaoFactory {
-    public static AgendaDao createAgendaDao(){
-        return new AgendaDaoJDBC(DB.getConnection());
 
-    }
     public static VendedoresDao createVendedoresDao() {
         return new VendedoresDaoJDBC(DB.getConnection());
     }
